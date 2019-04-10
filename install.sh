94e939d5f0389edb12f934b0361a9c1a2002047f
Artem Vang <vangogius@gmail.com>
2019-04-10 21:34:33 +0300

    Added installation script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+INSTALL_PATH=~/.markdown-dstu-builder
+
+mkdir -p $INSTALL_PATH
+
+cp Makefile dstu-references.csl template.tex $INSTALL_PATH
+cat md2dstu | sed -E "s|@install_path@|$INSTALL_PATH|g" > $INSTALL_PATH/md2dstu
+chmod +x $INSTALL_PATH/md2dstu
+echo "md2dstu successfully installed to $INSTALL_PATH"
+echo "Execute following line to update PATH environment variable"
+echo "echo 'export PATH=$INSTALL_PATH:\$PATH' >> ~/.bashrc && bash"
